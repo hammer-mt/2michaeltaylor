8900eafaedbf9a8b383d5e2af8e81301b172ca35
Michael Taylor <mjt145@gmail.com>
2015-08-09 15:59:58 -0400

    incorporated bootstrap template

--- core/views.py
+++ core/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
-class SplashView(TemplateView):
-     template_name = 'index.html'
+class LandingView(TemplateView):
+	template_name = 'base/index.html'
+
+class AboutView(TemplateView):
+	template_name = 'base/about.html'
+
+class ContactView(TemplateView):
+	template_name = 'base/contact.html'
+
+class PostView(TemplateView):
+	template_name = 'base/post.html'
